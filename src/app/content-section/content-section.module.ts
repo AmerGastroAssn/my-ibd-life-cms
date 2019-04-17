3919f4046a1cef71eea4af9fd0d23e4f9437e4bf
Colin Stodd <colperdev@gmail.com>
2019-04-17 16:41:59 -0400

    Added image dimentions

--- src/app/content-section/content-section.module.ts
+++ src/app/content-section/content-section.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { CKEditorModule } from 'ng2-ckeditor';
+import { ImageModule } from '../image/image.module';
 import { SharedModule } from '../shared/shared.module';
 import { CallToActionDetailComponent } from './components/call-to-action/call-to-action-detail/call-to-action-detail.component';
 import { CallToActionEditComponent } from './components/call-to-action/call-to-action-edit/call-to-action-edit.component';
@@ -32,6 +33,7 @@ import { TextSectionService } from './services/text-section.service';
         MatTooltipModule,
         ContentSectionRoutingModule,
         SharedModule,
+        ImageModule,
     ],
     declarations: [
         CallToActionComponent,
